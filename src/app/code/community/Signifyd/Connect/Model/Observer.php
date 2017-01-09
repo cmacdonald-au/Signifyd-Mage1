dac4ca33c1fd3a8c6e4aeeb0be6808fe9a327f71
colin <colin>
2017-01-09 13:33:22 +1100

    Adding setting to control "hold everything" functionality

--- src/app/code/community/Signifyd/Connect/Model/Observer.php
+++ src/app/code/community/Signifyd/Connect/Model/Observer.php
@@ -14,6 +14,7 @@ class Signifyd_Connect_Model_Observer extends Varien_Object
     public function openCase($observer)
     {
         try {
+
             if (!Mage::getStoreConfig('signifyd_connect/settings/enabled') && !$this->getEnabled()) {
                 return;
             }
@@ -21,18 +22,22 @@ class Signifyd_Connect_Model_Observer extends Varien_Object
             {
                 return;
             }
-            
+
             $event = $observer->getEvent();
-            
+
             if ($event->hasOrder()) {
                 $order = $event->getOrder();
             } else if ($event->hasObject()) {
                 $order = $event->getObject();
             }
-            
+
             $order_model = get_class(Mage::getModel('sales/order'));
-            
             if (!($order instanceof $order_model)) {
+                return;
+            }
+
+            if ($order->getShippingMethod() == 'm2eproshipping_m2eproshipping') {
+                Mage::log(__METHOD__.'() Skipping m2epro order');
                 return;
             }
 
@@ -43,11 +48,11 @@ class Signifyd_Connect_Model_Observer extends Varien_Object
         // If we get here, then we have failed to create the case.
 
     }
-    
+
     public function logData($order, $payment, $quote)
     {
         // Used to capture data for testing with
-        
+
         $order_data = json_encode($order->getData());
         $billing_data = json_encode($order->getBillingAddress()->getData());
         $shipping_data = json_encode($order->getShippingAddress()->getData());
@@ -56,16 +61,16 @@ class Signifyd_Connect_Model_Observer extends Varien_Object
         $quote_data = json_encode($quote->getData());
         $items = array();
         $products = array();
-        
+
         foreach ($quote->getAllItems() as $item) {
             $items[$item->getId()] = $item->getData();
             $product = Mage::getModel('catalog/product')->load($item->getProductId());
             $products[$item->getId()] = $product->getData();
         }
-        
+
         $items = json_encode($items);
         $products = json_encode($products);
-        
+
         Mage::log("Order:\n $order_data", null, 'signifyd_connect_objects.log');
         Mage::log("Billing:\n $billing_data", null, 'signifyd_connect_objects.log');
         Mage::log("Shipping:\n $shipping_data", null, 'signifyd_connect_objects.log');
@@ -75,11 +80,11 @@ class Signifyd_Connect_Model_Observer extends Varien_Object
         Mage::log("Items:\n $items", null, 'signifyd_connect_objects.log');
         Mage::log("Products:\n $products", null, 'signifyd_connect_objects.log');
     }
-    
+
     public function getAdminRoute()
     {
         $route = false;
-        
+
         try {
             // 1.4.0.0 support means we need to hard code these paths
             if ((bool)(string)Mage::getConfig()->getNode('default/admin/url/use_custom_path')) {
@@ -88,96 +93,96 @@ class Signifyd_Connect_Model_Observer extends Varien_Object
                 $route = Mage::getConfig()->getNode('admin/routers/adminhtml/args/frontName');
             }
         } catch (Exception $e) {
-            
+
         }
-        
+
         if (!$route) {
             $route = 'admin';
         }
-        
+
         return $route;
     }
-    
+
     public function eavCollectionAbstractLoadBefore($observer)
     {
         $x = $observer->getCollection();
-        
+
         $request = Mage::app()->getRequest();
         $module = $request->getModuleName();
         $controller = $request->getControllerName();
-        
+
         if ($module != $this->getAdminRoute() || $controller != 'sales_order') {
             return;
         }
-        
+
         $clss = get_class($x);
         if ($clss == 'Mage_Sales_Model_Mysql4_Order_Collection' || $clss == 'Mage_Sales_Model_Mysql4_Order_Grid_Collection') {
             $observer->setOrderGridCollection($x);
             return $this->salesOrderGridCollectionLoadBefore($observer);
         }
     }
-    
+
     public function coreCollectionAbstractLoadBefore($observer)
     {
         $x = $observer->getCollection();
-        
+
         $request = Mage::app()->getRequest();
         $module = $request->getModuleName();
         $controller = $request->getControllerName();
-        
+
         if ($module != $this->getAdminRoute() || $controller != 'sales_order') {
             return;
         }
-        
+
         $clss = get_class($x);
-        
+
         if ($clss == 'Mage_Sales_Model_Mysql4_Order_Collection' || $clss == 'Mage_Sales_Model_Mysql4_Order_Grid_Collection') {
             $observer->setOrderGridCollection($x);
             return $this->salesOrderGridCollectionLoadBefore($observer);
         }
     }
-    
+
     public function isCe()
     {
         return !@class_exists('Enterprise_Cms_Helper_Data');
     }
-    
+
     public function oldSupport()
     {
         $version = Mage::getVersion();
-        
+
         if ($this->isCe()) {
             return version_compare($version, '1.4.1.0', '<');
         } else {
             return version_compare($version, '1.10.0.0', '<');
         }
-        
+
         return false;
     }
-    
+
     public function belowSix()
     {
         $version = Mage::getVersion();
-        
+
         if ($this->isCe()) {
             return version_compare($version, '1.6.0.0', '<');
         } else {
             return version_compare($version, '1.11.0.0', '<');
         }
-        
+
         return false;
     }
-    
+
     public function salesOrderGridCollectionLoadBefore($observer)
     {
         $request = Mage::app()->getRequest();
         $module = $request->getModuleName();
         $controller = $request->getControllerName();
-        
+
         if ($module != $this->getAdminRoute() || $controller != 'sales_order') {
             return;
         }
-        
+
         $collection = $observer->getOrderGridCollection();
         $select = $collection->getSelect();
 
@@ -195,7 +200,7 @@ class Signifyd_Connect_Model_Observer extends Varien_Object
             $this->joins++;
         }
     }
-    
+
     public function coreBlockAbstractToHtmlBefore(Varien_Event_Observer $observer)
     {
         $request = Mage::app()->getRequest();
@@ -284,9 +289,19 @@ class Signifyd_Connect_Model_Observer extends Varien_Object
         if(!Mage::helper('signifyd_connect')->isEnabled()){
             return $this;
         }
+
+        if (!Mage::getStoreConfig('signifyd_connect/advanced/hold_all_orders')) {
+            return $this;
+        }
+
         $order = $observer->getEvent()->getOrder();
         if($order->canHold() === false){
             $this->helper->logError("Order {$order->getIncrementId()} could not be held because Magento returned false for canHold");
+        }
+
+        if ($order->getShippingMethod() == 'm2eproshipping_m2eproshipping') {
+            Mage::log(__METHOD__.'() Not holding m2epro order');
+            return $this;
         }
 
         try {
